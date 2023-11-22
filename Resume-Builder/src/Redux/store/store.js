780eb86f318b1c8f161cbd54fb4a0d29408cb822
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2023-11-23 03:23:37 +0600

    Delete Resume-Builder directory

--- Resume-Builder/src/Redux/store/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import rootReducer from '../reducers/rootReducer'
-
-export const store = createStore(
-  rootReducer,
-  compose(applyMiddleware(thunk))
-)
